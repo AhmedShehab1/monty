9ddbd4f87c5e8d62ed8848d9d2b3778eb64ff2dd
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-31 21:07:46 +0200

    .

--- stack_f2.c
+++ stack_f2.c
@@ -21,7 +21,7 @@ void pop(stack_t **stack, int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%d: : can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
